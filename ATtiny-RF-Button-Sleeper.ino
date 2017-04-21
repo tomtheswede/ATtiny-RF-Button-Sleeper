3118313d0efac873b6d9611884bcb4538a911763
Thomas Friberg <tomtheswede@gmail.com>
2017-04-21 14:28:45 +0800

    Change to interupt based switching

--- ATtiny-RF-Button-Sleeper.ino
+++ ATtiny-RF-Button-Sleeper.ino
@@ -9,35 +9,41 @@ const unsigned long devID = 183322503; // 00001010111011011000100111101111 So th
 const unsigned long devType = 1; //Reads as "1" corresponding with BTN type
 
 //General variables
-const byte sendPin = 2; //RF pin. pin 3 for rfbutton v0.4 which is pb4 so use 4
 const byte pwrPin = 1; //Power for external elements pin. pin 6 for rfbutton v0.4 pin 6 is PB1 so use 1
+
+//RF related
+const byte sendPin = 2; //RF pin. pin 3 for rfbutton v0.4 which is pb4 so use 4
 const byte typePreamble[4] = {120,121,122,123}; //01111000 for register, 01111001 for basic message type
-long lastTrigger=millis();
-bool longPressPrimer=true;
-bool longerPressPrimer=true;
-bool longestPressPrimer=true;
-byte msgType=1;
 byte msgLengths[4]={32,8,16,32};
 byte msgBuffer[9]; //Max 9 bytes for transmission
 int msgLength;
-int k;
 
 //Button related
-const byte btnPin = 0; //rfButton v0.4 uses pin 5 which is PB0 so use 0 here.
-bool lastButtonState=0;
-bool buttonState=0;
-long buttonTriggerTime=millis();
 long currentTime=millis();
-bool primer[4]={0,0,0,0};
+
+//Interrupt variables
+#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit)) //OR
+#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit)) //AND
+volatile bool primer[5]={0,0,0,0,0};
+volatile bool pressed=0;
+volatile bool buttonState=0;
+volatile unsigned long pressTime=0;
+volatile const unsigned int reTriggerDelay=50; //minimum time in millis between button presses to remove bad contact
+volatile const byte btnPin = 0; //rfButton v0.4 uses pin 5 which is PB0 so use 0 here.
+
 
 void setup() { //This code runs as soon as the device is powered on.
   pinMode(sendPin,OUTPUT);
   pinMode(pwrPin,OUTPUT);
   digitalWrite(pwrPin,1);
   pinMode(btnPin,INPUT_PULLUP);
-  //PORTB &= ~(1<<PB0);
 
   encodeMessage(0,devID); //Register on first on
+
+  digitalWrite(pwrPin,0); //Power down devices between button pushes
+  //Set pin to start interrupts
+  sbi(GIMSK,PCIE); //Turn on interrupt
+  sbi(PCMSK,PCINT0); //set pin affected by interupt
 }
 
 void loop() {
@@ -46,43 +52,60 @@ void loop() {
 
 }
 
-void CheckButton() {
+//Button functions --------------------------------------------
+
+ISR(PCINT0_vect) {
+  // This is called when the interrupt occurs
   buttonState=!(digitalRead(btnPin));
-  currentTime=millis();
-  if (buttonState!=lastButtonState) {
-    if (buttonState && currentTime-buttonTriggerTime>300) {
-      encodeMessage(1,0);
-      buttonTriggerTime=currentTime;
-      primer[0]=1;
-      primer[1]=1;
-      primer[2]=1;
-      primer[3]=1;
-    }
-    else if (!buttonState) {
-      primer[0]=0;
-      primer[1]=0;
-      primer[2]=0;
-      primer[3]=0;
-    }
-  }
-  lastButtonState=buttonState;
-  if (primer[0] && (currentTime-buttonTriggerTime>600)) {
-    encodeMessage(1,1);
-    primer[0]=0;
-  }
-  else if (primer[1] && (currentTime-buttonTriggerTime>1500)) {
-    encodeMessage(1,2);
-    primer[1]=0;
-  }
-  else if (primer[2] && (currentTime-buttonTriggerTime>4000)) {
-    encodeMessage(1,3);
-    primer[2]=0;
-  }
-  else if (primer[3] && (currentTime-buttonTriggerTime>8000)) {
-    encodeMessage(0,devID);  //Register
-    primer[3]=0;
+  if (buttonState && (millis()-pressTime>reTriggerDelay)) { //if pressed in
+    pressed=true;
+    primer[0]=1;
+    primer[1]=1;
+    primer[2]=1;
+    primer[3]=1;
+    primer[4]=1;
+    pressTime=millis();
+    //encodeMessage(1,5);
   }
 }
+
+void CheckButton() {
+  if (pressed && digitalRead(btnPin)) {
+    pressed = false;
+    digitalWrite(pwrPin,0); //Turn off ancillaries
+    primer[0]=0;
+    primer[1]=0;
+    primer[2]=0;
+    primer[3]=0;
+    primer[4]=0;
+  }
+  if (pressed) {
+    digitalWrite(pwrPin,1); //Turn on ancillaries
+    currentTime=millis();
+    if (primer[0]) {
+      encodeMessage(1,1);
+      primer[0]=0;
+    }
+    else if (primer[1] && (currentTime-pressTime>600)) {
+      encodeMessage(1,2);
+      primer[1]=0;
+    }
+    else if (primer[2] && (currentTime-pressTime>1500)) {
+      encodeMessage(1,3);
+      primer[2]=0;
+    }
+    else if (primer[3] && (currentTime-pressTime>4000)) {
+      encodeMessage(1,4);
+      primer[3]=0;
+    }
+    else if (primer[4] && (currentTime-pressTime>8000)) {
+      encodeMessage(0,devID);  //Register
+      primer[4]=0;
+    }
+  }
+}
+
+//RF Functions ------------------------------------------------------------------------
 
 void pulse(bool logic) {
   if (logic) {
@@ -100,6 +123,7 @@ void pulse(bool logic) {
 }
 
 void encodeMessage(byte msgType,unsigned long msg) {
+  int k;
   msgLength=msgLengths[msgType];
   k=0;
   //construct the message
